7e00797e3b364a9e2a637c052062e81fce4268f5
ArthurFranceschini <arthurfranvalerio@gmail.com>
2025-10-20 14:11:32 -0300

    indexar botões

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from "@astrojs/tailwind";
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
-  integrations: [tailwind()],
+  site: "https://drescritura.com.br",
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: vercel(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        grotesk: ["Grotesk", "sans-serif"],
+        grotesk: ["Montserrat", "sans-serif"],
       },
       fontWeight: {
         regular: 400,
